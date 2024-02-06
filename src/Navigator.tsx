4ab7a054936535242282d502510ba8cb5d860014
Ivan Bolaños <ivan.bolanos@dormakaba.com>
2024-02-06 21:54:24 +0100

    git init project

---
+++ src/Navigator.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+
+const Navigator = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Text>About</Text>
+      <Text>Contact</Text>
+    </View>
+  );
+};
+export default Navigator;
